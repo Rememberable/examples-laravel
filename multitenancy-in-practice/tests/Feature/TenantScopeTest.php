7f9deb79ca31d75db754c6062917efe466536994
= <=>
2022-12-20 20:38:29 +0300

    Fix errors

--- multitenancy-in-practice/tests/Feature/TenantScopeTest.php
+++ multitenancy-in-practice/tests/Feature/TenantScopeTest.php
@@ -11,7 +11,6 @@ use Tests\TestCase;
 
 class TenantScopeTest extends TestCase
 {
-    use RefreshDatabase, WithFaker;
     /** @test */
     public function a_model_has_a_tenant_id_on_the_migration()
     {
